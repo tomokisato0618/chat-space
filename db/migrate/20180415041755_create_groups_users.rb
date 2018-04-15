1e975309e70ecf0552bc6cb98e7d70b19b878a2c
tomokisato0618 <kotsu@mba.local>
2018-04-15 15:14:20 +0900

    Define groups and groups_users table
    ・groupsテーブル構造の定義
    ・groups_usersテーブル構造の定義
    
    (注意)
    ・groupsテーブルnameカラムにindexの付与
     　→カリキュラムとの相違を防ぐため
    ・groups_usersテーブルのreferences型のカラムにindex: trueを付与
    　→カリキュラムと相違がある点のため注意

--- db/migrate/20180415041755_create_groups_users.rb
+++ db/migrate/20180415041755_create_groups_users.rb
@@ -1,7 +1,8 @@
 class CreateGroupsUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :groups_users do |t|
-
+      t.references  :group, foreign_key: true, index: true
+      t.references  :user, foreign_key: true, index: true
       t.timestamps
     end
   end
