1e682a3076eea72c6a052eb5d17e25af8d90a6dc
tomokisato0618 <kotsu@mba.local>
2018-04-25 01:45:56 +0900

    Modify messages factory file
    ・messageモデル用のファクトリファイルの修正

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :message do
-    context Faker::Lorem.sentence
+    content Faker::Lorem.sentence
     image   File.open("#{Rails.root}/public/images/kiyoshi1.jpg")
     user
     group
